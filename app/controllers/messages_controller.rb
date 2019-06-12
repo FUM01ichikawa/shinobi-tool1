429e8993e456e99153907f5c634d2a87062b0485
市川史章 <kiyoshi.kassember@gmail.com>
2019-06-12 17:17:10 +0900

    Create_routes&simple_views
    # What
    ・基本的なrouteの設定
    ・基本となる各viewを作成
    
    # Why
    ・具体的な機能の実装に向けて必要となるため

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,6 +7,27 @@ class MessagesController < ApplicationController
   end
 
   def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+      if @message.save
+        redirect_to root_path, notice: 'グループを作成しました'
+      else
+        render :new
+      end
+  end
+
+  def show
+  end
+
+  def serch
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:original_text)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :messages, only: [:index, :new, :create,:show]
-  resources :ocr_iamges, only: [:index, :new, :create,:show]
-  root to: 'messages#index'
+  resources :messages
+  resources :ocr_images
+  root to: 'ocr_images#index'
 
 end
