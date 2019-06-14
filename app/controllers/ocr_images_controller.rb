9ff6010c271b9414cceb45d4a22c19c312d00942
市川史章 <kiyoshi.kassember@gmail.com>
2019-06-14 12:41:40 +0900

    Develop-ocr-image-function
    # what
    ・メイン機能であるgoogle cloud visionのapiを活用したOCR(光学文字認識)でのテキスト検出→データ保存の機能を実装
    
    #why
    ・アプリケーションによるメインの機能となるため

--- app/controllers/ocr_images_controller.rb
+++ app/controllers/ocr_images_controller.rb
@@ -1,5 +1,6 @@
+
 class OcrImagesController < ApplicationController
-  
+  require "google/cloud/vision"
   def index
   end
 
@@ -9,18 +10,24 @@ class OcrImagesController < ApplicationController
 
   def create
     @ocr_image = OcrImage.new(ocr_image_params)
-      if @ocr_image.save
-        redirect_to root_path, notice: 'グループを作成しました'
-      else
+    
+    if @ocr_image.save
+      ocr_image_id = @ocr_image[:id]
+      ocr_image_name = @ocr_image[:name]
+      response_text = GoogleCloudVision.ocr_result(ocr_image_id,ocr_image_name)
+      @ocr_image.update(ocr_result:response_text)
+    else
         render :new
       end
   end
 
   def show
   end
-end
+
 
 private
   def ocr_image_params
-    params.require(:ocr_image).permit(:name)
+    params.require(:ocr_image).permit(:name,:ocr_result)
   end
+
+end
