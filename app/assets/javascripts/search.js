81181eb6707907c511c26c8392fe002064a07d41
市川史章 <kiyoshi.kassember@gmail.com>
2019-06-23 15:13:35 +0900

    Sub-function-Copy-button
    # What
    ・作成した暗号並びに解読したオリジナルテキストをCopyボタンでコピーできるようにする
    
    # Why
    ・暗号の作成→解読をスムーズに行えるようにするため。
    ・基本機能の各ボタンの導線を完了させるため。

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,41 @@
+function execCopy(string){
+
+  // 空div 生成
+  var tmp = document.createElement("div");
+  // 選択用のタグ生成
+  var pre = document.createElement('pre');
+
+  // 親要素のCSSで user-select: none だとコピーできないので書き換える
+  pre.style.webkitUserSelect = 'auto';
+  pre.style.userSelect = 'auto';
+
+  tmp.appendChild(pre).textContent = string;
+
+
+  // body に追加
+  document.body.appendChild(tmp);
+  // 要素を選択
+  document.getSelection().selectAllChildren(tmp);
+
+  // クリップボードにコピー
+  var result = document.execCommand("copy");
+
+  // 要素削除
+  document.body.removeChild(tmp);
+
+  return result;
+}
+
+
+var original_text = document.getElementById('original_text').textContent;
+var text_copy = document.getElementById('text_copy');
+console.log(text_copy);
+
+text_copy.onclick = function(){
+  if(execCopy(original_text)){
+    alert('解読したオリジナルテキストをコピーしました');
+  }
+  else {
+    alert('このブラウザでは対応していません');
+  }
+};
